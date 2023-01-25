15e8ab2cbaeb09c070cb84cb5664c174231fd87a
Anupa Benny <81522640+anupabenny@users.noreply.github.com>
2023-01-25 18:54:15 +0530

    Update dev_run_backingsvcs.sh

--- dev_run_backingsvcs.sh
+++ dev_run_backingsvcs.sh
@@ -6,8 +6,8 @@
 # alias devRedis="docker rm -f devRedis ; docker run -d --name devRedis -p 6379:6379 redis"
 
 
-docker rm -f devRabbit ; docker run -d --name devRabbit -p 5672:5672 -p 5673:5673 -p 15672:15672 rabbitmq:latest
-docker rm -f devPostgres ; docker run -d --name devPostgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres postgres:latest
-docker rm -f devRedis ; docker run -d --name devRedis -p 6379:6379 redis:latest
+docker run -d --name devRabbit -p 5672:5672 -p 5673:5673 -p 15672:15672 rabbitmq:latest
+docker run -d --name devPostgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres postgres:latest
+docker run -d --name devRedis -p 6379:6379 redis:latest
 
 echo all services started
