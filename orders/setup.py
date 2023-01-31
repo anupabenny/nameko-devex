2d7427d9db688ce560c03d4fa855ca75d5e77514
Anupa Benny <81522640+anupabenny@users.noreply.github.com>
2023-01-31 08:07:30 +0530

    Update setup.py

--- orders/setup.py
+++ orders/setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'nameko==v3.0.0-rc6',
         'nameko-sqlalchemy==1.5.0',
-        'alembic==1.0.10',
+        'alembic==1.9.0',
         'marshmallow==2.19.2',
         'psycopg2-binary==2.9.3',
     ],
